d5ba6b6670c61e30d7f096b93c76d60b28d7bf4a
epten08 <epten08@gmail.com>
2024-01-24 08:32:34 +0200

    schemas updated

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,11 @@
-export const schemaTypes = []
+import brands from "./brands";
+import testimonials from "./testimonials";
+import abouts from "./abouts";
+import contact from "./contact";
+import skills from "./skills";
+import works from "./works";
+import WorkExperience from "./workExperience";
+import Expriences from "./experiences";
+import experiences from "./experiences";
+
+export const schemaTypes = [testimonials, abouts, brands, contact, skills, works, WorkExperience, experiences]
